6484e7f8867467285dce2072dfae7f7e84756922
hmbendale21 <himanshubendale88@gmail.com>
2026-07-15 13:58:31 +0530

    feat: add hero typing animation

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+var typed = new Typed("#typed-text", {
+    strings: [
+        "Protect Your Data.",
+        "Detect Cyber Threats.",
+        "Defend Against Attacks.",
+        "Learn Cybersecurity."
+    ],
+    typeSpeed: 60,
+    backSpeed: 40,
+    backDelay: 1500,
+    loop: true
+});
